fc1a94924c4cb9946a407ff9d93bcfa435fd16fc
galangekwyn <ggalangekawiyonoo@gmail.com>
2023-08-16 13:10:50 +0700

    Modified file

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    specPattern : "cypress/support/e2e",
+    specPattern : "cypress/support/e2e.js",
     supportFile : false,
     },
   },
